b639af8537df075d0ce3188fc2483984375f50f1
kkingsbe <kkingsbe@gmail.com>
2022-12-25 11:55:20 -0500

    Added support for clearing cache

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const axios = require("axios")
 
 //const baseurl = "https://www.recache.cloud/api"
-const baseurl = "http://localhost:3001/api"
-let key
+const baseurl = "http://localhost:3000/api"
+let key, projectId
 
 class Recache {
-    static init(_key) {
-        //Store key within module
+    /**
+     * Initializes the Recache module
+     * @param {*} _key Your project authentication token
+     * @param {*} _projectId The ID of the Recache project which is being accessed
+     */
+    static init(_key, _projectId) {
         key = _key
+        projectId = _projectId
     }
 
-    static async cached(projectId, endpointId, func, args, compare) {
+    /**
+     * Performs a cached function call
+     * @param {*} endpointId The endpoint ID
+     * @param {*} func The function to run
+     * @param {*} args An array of the arguments to pass into the function
+     * @param {*} compare A slot for additional JSON which can be used for matching against the cache (if you have an endpoint that returns a different response depending on which user is currently signed in, you can pass the User object in to this argument)
+     * @returns The return value from the executed function
+     */
+    static async cached(endpointId, func, args, compare) {
         if(typeof(compare) == "undefined") compare = ""
         //See if there is already a cached call with same id and arguments
         try {
@@ -35,6 +48,39 @@ class Recache {
             if(e?.response?.data == '"Endpoint not found"') console.log("     Incorrect token, project ID, or endpoint ID. Please verify configuration.")
             else console.log(e.stack)
             return await func(...args)
+        }
+    }
+
+    /**
+     * Clears the cache for the given endpoint in your project
+     * @param {*} endpointId 
+     */
+    static async clearEndpointCache(endpointId) {
+        try {
+            await axios.get(`${baseurl}/projects/${projectId}/${endpointId}/flush?token=${key}`)
+        } catch (e) {
+            console.log("Recache: Issue with our servers or your configuration.")
+            if(e?.response?.status == 403) {
+                console.log("     Incorrect project id, token, or endpoint id")
+            } else {
+                console.log(e.stack)
+            }
+        }
+    }
+
+    /**
+     * Clears the cache for your project 
+     */
+    static async clearProjectCache() {
+        try {
+            await axios.get(`${baseurl}/projects/${projectId}/flush?token=${key}`)
+        } catch (e) {
+            console.log("Recache: Issue with our servers or your configuration.")
+            if(e?.response?.status == 403) {
+                console.log("     Incorrect project id or token")
+            } else {
+                console.log(e.stack)
+            }
         }
     }
 }
