17bafccb1c626d1dadd168842aa6438f886316f2
kkingsbe <kkingsbe@gmail.com>
2023-01-20 13:52:08 -0500

    Hotfix

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ class Recache {
             await axios.post(`${baseurl}/projects/${projectId}/wsmessages/add`, {
                 ip: ws._socket.remoteAddress,
                 message,
-                token
+                key
             })
         } catch (e) {
             console.log("Recache: Issue with our servers or your configuration.")
